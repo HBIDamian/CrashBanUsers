16807d390da411a76e83fd0ed1404565256c778a
HBIDamian <10010369+HBIDamian@users.noreply.github.com>
2022-07-19 23:33:13 +0100

    Added Unban command. Now it's release ready

--- src/HBIDamian/CrashBanUsers/Main.php
+++ src/HBIDamian/CrashBanUsers/Main.php
@@ -1,10 +1,16 @@
 <?php
 namespace HBIDamian\CrashBanUsers;
 
+use InvalidArgumentException;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\nbt\NBT;
+use pocketmine\nbt\tag\DoubleTag;
+use pocketmine\nbt\tag\FloatTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\nbt\tag\StringTag;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
@@ -21,9 +27,12 @@ protected Config $bannedUsersDB;
 	}
 
 	public function onPlayerLogin(PlayerLoginEvent $event): void {
-		if($this->bannedUsersDB->exists($event->getPlayer()->getName())){
-			$pos = new Position(0, -2147483648, 0, $this->getServer()->getWorldManager()->getDefaultWorld());
+		if($this->bannedUsersDB->exists(strtolower($event->getPlayer()->getName()))){
+			$defaultWorld = $this->getServer()->getWorldManager()->getDefaultWorld();
+			$pos = new Position($defaultWorld->getSafeSpawn()->getX(), -2147483648, $defaultWorld->getSafeSpawn()->getZ(), $defaultWorld);
 			$event->getPlayer()->teleport($pos);
+			// Teleports the player to x32 Signed Integer's minimum value.
+			// The client seems to crash if you teleport at that point.
 		}
 	}
 
@@ -38,11 +47,41 @@ protected Config $bannedUsersDB;
 				$sender->sendMessage(TextFormat::RED . "Player not found.");
 				return true;
 			}
-			$this->bannedUsersDB->set($player->getName());
+
+			$this->bannedUsersDB->set(strtolower($player->getName()), "banned by " . $sender->getName());
 			$this->bannedUsersDB->save();
-			$player->kick("Your account is banned. Attempts to rejoin will crash your game.", false);
+			$player->kick("Your account is banned.", false);
 			$sender->sendMessage(TextFormat::GREEN . "Player " . TextFormat::YELLOW . $player->getName() . TextFormat::GREEN . " has been banned.");
 			return true;
+			
+		} elseif ($command->getName() == "crashunban"){
+			if(!isset($args[0])){
+				$sender->sendMessage(TextFormat::RED . "Usage: /crashunban <player>");
+				return true;
+			}
+
+			$safeSpawn = $this->getServer()->getWorldManager()->getDefaultWorld()->getSafeSpawn();
+			$player = $this->getServer()->getOfflinePlayer($args[0])->getName();
+			if($this->getServer()->getOfflinePlayerData($player) == null){
+				$sender->sendMessage(TextFormat::YELLOW . $player. TextFormat::RED . " not found in the players directory.");
+				return true;
+			}
+
+			$nbt = $this->getServer()->getOfflinePlayerData($player);
+			$nbt->setTag("Pos", new ListTag([
+				new DoubleTag($safeSpawn->getX()),
+				new DoubleTag($safeSpawn->getY()),
+				new DoubleTag($safeSpawn->getZ())
+			], NBT::TAG_Double));
+			$this->getServer()->saveOfflinePlayerData($player, $nbt);
+
+			if($this->bannedUsersDB->exists($player)){
+				$this->bannedUsersDB->remove($player);
+				$this->bannedUsersDB->save();
+				$sender->sendMessage(TextFormat::GREEN . "Player " . TextFormat::YELLOW . $player . TextFormat::GREEN . " has been unbanned.");
+			} else {
+				$sender->sendMessage(TextFormat::RED . "Player " . TextFormat::YELLOW . $player . TextFormat::RED . " is not banned.");
+			}
 		}
 		return true;
 	}
